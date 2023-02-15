27bdd788179100856e0cbb803eb7d43f8108d6e7
lvzl <627417163@qq.com>
2023-02-15 21:55:43 +0800

    feat: font

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,5 @@ export default {
 
 <style>
 /*每个页面公共css */
+@import url(./style/iconfont.css);
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,53 +3,21 @@
     <view class="status_bar">
       <!-- 这里是状态栏 -->
     </view>
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title" @click="store.count++">{{ store.count }}</text>
-    </view>
+    <text class="icon-yonghu"></text>
   </view>
 </template>
 
-<script>
+<script setup>
 import { useCounterStore } from '@/store/main/index.js'
-export default {
-  data() {
-    return {
-      title: 'Hello',
-      store: useCounterStore()
-    }
-  },
-  onLoad() { },
-  methods: {},
+
+const store = useCounterStore()
+
+function onLoad() {
+
 }
 </script>
 
 <style lang="scss">
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: $uni-color-primary;
-}
 .status_bar {
   height: var(--status-bar-height);
   width: 100%;

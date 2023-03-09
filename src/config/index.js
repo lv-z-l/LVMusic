1793b1b81e927f0d09348b566899db9f5cee470e
lvzl <627417163@qq.com>
2023-03-09 10:29:04 +0800

    fix: 换个图标

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ export default {
     icons: [
       { icon: 'icon-phb', text: 'recommend', comp: 'recommend' },
       { icon: 'icon-24gf-playCircle', text: 'category', comp: 'category' },
-      { icon: 'icon-wode', text: 'mine', comp: 'mine' },
+      { icon: 'icon-yonghu', text: 'mine', comp: 'mine' },
       { icon: 'icon-sousuo', text: 'search', comp: 'search' }
     ]
   },
